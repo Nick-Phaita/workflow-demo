9f7821dd55a56ff150ac9c9d8a12c727978b34b0
Nick-Phaita <nickphaita@gmail.com>
2024-10-08 19:34:59 +0300

    Modified index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Welcome to my PHP App!";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubutntu Server serving</title>
+</head>
+<body>
+    <h2>Welcome to our GitHub Workflow test application</h2>
+    <p>This project has been worked on by:</p>
+    <ul>
+        <li>Nick Maina</li>
+    </ul>
+</body>
+</html>
