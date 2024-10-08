ad3c86aea6a27ec200db5c58dad6f9bba1c22d1c
Nick-Phaita <nickphaita@gmail.com>
2024-10-08 19:30:37 +0300

    Revert "Added login/registration code"
    This reverts commit bbaf17ecf11208c83ed1a4d01c491efce8eaaf28.

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP App</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Welcome to My PHP App!</h1>
-        <a href="register.php">Register</a>
-        <a href="login.php">Login</a>
-    </div>
-</body>
-</html>
-
+<?php
+echo "Welcome to my PHP App!";
+?>
--- src/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = new PDO("mysql:host=localhost;dbname=workflow_demo", "root", "root");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
-
